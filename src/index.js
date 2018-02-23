67246e63388adee6f82eff2637330c18c06ca5ed
Grant Sykes <grantrjsykes@googlemail.com>
2018-02-23 07:47:01 +0000

    Alpha version
    version 1.0a

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get("/", (request, response) => {
+	response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+
+	socket.on('disconnect', () => {
+		console.log('a user disconnected');
+	});
+
+	socket.on('chat message', (message) => {
+		console.log('message: ' + message);
+		io.emit('chat message', message);
+	});
+});
+
+http.listen(3000, () => {
+	console.log('listening on *:3000');
+});
